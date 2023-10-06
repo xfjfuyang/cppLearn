41eebf3885db1a6a30db7551446eb082f72bef9a
xfjfuyang <2426263017@qq.com>
2023-10-06 21:52:58 +0800

    add common include file

---
+++ include/comm.h
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include <iostream>
+#include <stdint.h>
+
+#include <vector>
+#include <list>
+#include <array>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+
+#include <mutex>
+#include <shared_mutex>
+#include <thread>
+#include <chrono>
+
+#include <memory>
+
+#include <algorithm>
+
+#include <math.h>
+
+//using namespace std;
